2f5f8865b6c8c12ff5d83ca17ae1b0bba5b56b11
SabaAbbas12 <sabaabbas286@gmail.com>
2019-05-09 11:20:44 +0500

    Program

---
+++ Ragged.java
@@ -0,0 +1,20 @@
+class Ragged {
+public static void main(String args[]) {
+      
+      int i,j ;
+         for(i=0;i<5;i++)
+             for(j=0;j<10;j++)
+      i = i + j ;
+         for(i=0 ;i<6 ; i++)
+             for( j=0; j<2; j++)
+      i = i + j ;
+   System.out.println( "ragged per trip during the week");
+          for( i=0; i<5; i++)
+             for( j=0; j<10; j++)
+      System.out.print(i);
+       System.out.println();
+}
+     
+ }
+
+  
